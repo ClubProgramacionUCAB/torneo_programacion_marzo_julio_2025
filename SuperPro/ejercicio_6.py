87e56d5e27c4b1143b4e337ccdca8897d85bbe24
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-05-27 15:58:22 -0400

    Update ejercicio_6.py

--- SuperPro/ejercicio_6.py
+++ SuperPro/ejercicio_6.py
@@ -1,19 +1,10 @@
-def container(height):
-    height = [int(n) for n in height]
-    p1 = 0
-    p2 = len(height) - 1
+def sol(arr):
+    m = 0
+    arr = [int(n) for n in arr]
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            m = max(m, (j - i)*min(arr[i], arr[j]))
+    return m
 
-    res = 0
 
-    for i in range(len(height)):
-        res = max(res, (p2 - p1) * min(height[p1], height[p2]))
-        if p1 > p2:
-            break
-        if height[p1] - height[p1 + 1] < height[p2] - height[p2 - 1]:
-            p1 += 1
-        else:
-            p2 -= 1
-
-    return res
-    
-print(container(input().split(',')))
+print(sol(input().split(',')))
