9fb145bf639dc2577d3be6ae7091ea3e4f7ade08
Juan Diego Mendoza Guardia <71406541+juandi46XD@users.noreply.github.com>
2025-05-27 15:06:09 -0400

    Add files via upload

---
+++ Anzoatigueños/Ejercicio5.py
@@ -0,0 +1,11 @@
+dividendo = int (input("Ingrese el dividendo: "))
+divisor = int (input("Ingrese el divisor: "))
+i = 0
+suma = abs (dividendo)
+while (suma - abs(divisor) > 0):
+    suma -= abs(divisor)
+    i += 1
+if ((divisor > 0 and dividendo < 0) or (divisor < 0 and dividendo > 0)):
+    print ("-", i)
+else:
+    print (i)
