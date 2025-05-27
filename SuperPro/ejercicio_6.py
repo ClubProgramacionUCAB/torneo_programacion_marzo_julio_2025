e5a5302e9b8aac1e0d6eadcee0efc997307a5c03
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-05-27 15:11:15 -0400

    Create ejercicio_6.py

---
+++ SuperPro/ejercicio_6.py
@@ -0,0 +1,18 @@
+def container(height):
+    height = [int(n) for n in height]
+    p1 = 0
+    p2 = len(height) - 1
+
+    res = 0
+
+    for i in range(len(height)):
+        res = max(res, (p2 - p1) * min(height[p1], height[p2]))
+        if height[p1] - height[p1 + 1] < height[p2] - height[p2 - 1]:
+            p1 += 1
+        else:
+            p2 -= 1
+
+    return res
+
+
+print(container(input().split(',')))
