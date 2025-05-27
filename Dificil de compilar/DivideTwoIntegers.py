3d603c69648cfe7ae213e8592b1061f8ac35b7a9
Freddy Fernandez <146951144+fafernandez24@users.noreply.github.com>
2025-05-27 15:36:49 -0400

    Update DivideTwoIntegers.py

--- Dificil de compilar/DivideTwoIntegers.py
+++ Dificil de compilar/DivideTwoIntegers.py
@@ -3,13 +3,16 @@ class Solution:
         res = dividendo
         cont = 0
         hola = divisor
-        if(divisor<0):
-            divisor=divisor-divisor-divisor
-        while(res >=divisor):
-            cont = cont + 1
-            res = res - divisor
-        if (hola<0):
-            cont=cont-cont-cont
-        print(cont)
+        if(divisor==0):
+            print("error")
+        else:
+            if(divisor<0):
+                divisor=divisor-divisor-divisor
+            while(res >=divisor):
+                cont = cont + 1
+                res = res - divisor
+            if (hola<0):
+                cont=cont-cont-cont
+            print(cont)
 
-Solution.DivideTwoIntegers(7,3)
+Solution.DivideTwoIntegers(7,-1)
