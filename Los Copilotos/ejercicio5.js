e8d952f76c6438145b5f38b7742ab99cf4e39945
Poletron <65890946+Poletron@users.noreply.github.com>
2025-05-27 16:08:52 -0400

    Update ejercicio5.js

--- Los Copilotos/ejercicio5.js
+++ Los Copilotos/ejercicio5.js
@@ -1,12 +1,17 @@
 function ejercicio5(dividend, divisor) {
+
     let quotient = 0;
-    let sign = dividend * divisor < 0 ? -1 : 1;
+    if ((dividend >= 0 && divisor > 0) || (dividend <= 0 && divisor < 0)) {
+        sign = 1;
+    }
+    else {
+        sign = -1;
+    }
     dividend = Math.abs(dividend);
     divisor = Math.abs(divisor);
     while (dividend > divisor) {
         dividend = dividend - divisor;
         quotient++;
     }
-
     return quotient * sign;
 }
