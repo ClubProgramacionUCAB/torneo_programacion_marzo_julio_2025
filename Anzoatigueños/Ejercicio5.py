3acf942095e25fa3d7c3b679a3bc8bbaff96bb6a
Juan Diego Mendoza Guardia <71406541+juandi46XD@users.noreply.github.com>
2025-05-27 15:50:02 -0400

    Ejercicio5.py

--- Anzoatigueños/Ejercicio5.py
+++ Anzoatigueños/Ejercicio5.py
@@ -2,10 +2,10 @@ dividendo = int (input())
 divisor = int (input())
 i = 0
 suma = abs (dividendo)
-while (suma - abs(divisor) > 0):
+while (suma - abs(divisor)>= 0):
     suma -= abs(divisor)
     i += 1
 if ((divisor > 0 and dividendo < 0) or (divisor < 0 and dividendo > 0)):
-    print ("-", i)
+    print (-i)
 else:
     print (i)
