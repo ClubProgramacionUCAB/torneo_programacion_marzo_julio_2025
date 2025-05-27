f13fd80a52be33be4556f6dd964fe8c4118b6101
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-05-27 15:29:39 -0400

    Update ejercicio_6.py

--- SuperPro/ejercicio_6.py
+++ SuperPro/ejercicio_6.py
@@ -7,12 +7,13 @@ def container(height):
 
     for i in range(len(height)):
         res = max(res, (p2 - p1) * min(height[p1], height[p2]))
+        if p1 > p2:
+            break
         if height[p1] - height[p1 + 1] < height[p2] - height[p2 - 1]:
             p1 += 1
         else:
             p2 -= 1
 
     return res
-
-
+    
 print(container(input().split(',')))
