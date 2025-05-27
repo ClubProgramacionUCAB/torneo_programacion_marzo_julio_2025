3c9c02324370af3be26a75879f446eeba73367fa
Freddy Fernandez <146951144+fafernandez24@users.noreply.github.com>
2025-05-27 15:24:32 -0400

    Add files via upload

---
+++ Dificil de compilar/DivideTwoIntegers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def DivideTwoIntegers(dividendo, divisor):
+        res = dividendo
+        cont = 0
+        hola = divisor
+        if(divisor<0):
+            divisor=divisor-divisor-divisor
+        while(res >=divisor):
+            cont = cont + 1
+            res = res - divisor
+        if (hola<0):
+            cont=cont-cont-cont
+        print(cont)
+
+Solution.DivideTwoIntegers(7,3)
