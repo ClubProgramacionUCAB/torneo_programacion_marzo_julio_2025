ccb68ab96f7693bac6b8fed69b62f6c2b093a749
Freddy Fernandez <146951144+fafernandez24@users.noreply.github.com>
2025-05-27 14:32:31 -0400

    RomanToInteger.py

--- Dificil de compilar/RomanToInteger.py
+++ Dificil de compilar/RomanToInteger.py
@@ -1,6 +1,19 @@
-class Roman():
-    def RomanToInteger():
-        numeroRomano = []
+class Solution:
+    def RomanToInteger(cadena):
+        respuesta = 0
+        numerosRomanos = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        for i in range(0,len(cadena),1):
+            letra = cadena[i]
+            vletra=numerosRomanos[letra]
+            if (i != len(cadena)-1):
+                sig= cadena[i+1]
+                sletra = numerosRomanos[sig]
+                if (vletra >= sletra):
+                    respuesta += vletra
+                else: 
+                    respuesta -= vletra
+            else:
+                respuesta += vletra
 
-    
-Roman.RomanToInteger
+        print(respuesta)
+Solution.RomanToInteger("XLD")
